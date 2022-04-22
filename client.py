f2c03496e82105c6a49fee3d595a2ff62b915bdd
sepjal <70891789+sepjal@users.noreply.github.com>
2022-04-22 13:29:44 +0100

    Update client.py

--- client.py
+++ client.py
@@ -12,8 +12,8 @@ from sklearn.datasets import load_digits
 
 
 def classify_digit(image):
-    # this function trains a classifier on sklearn digits dataset, and then trains a support vector machine
-    # once trained, it will classify the input 'image' and returns the predicted digit
+    # this function loads sklearn digits dataset, and then trains a support vector machine classifier
+    # once trained, it will classify the input 'image' and returns the predicted digit in string format
     # the trianed classifier can be saved on disk to be reused but as it is fast to train, and not the focus here, it is not saved
     digits = load_digits()
     sample_image = digits.images[0]
@@ -33,7 +33,7 @@ def classify_digit(image):
     clf.fit(X_train, y_train)
     # this model can  be saved on the disk and loaded next time to save time, however it is not necessary as it runs fast
 
-    # Predict the value of the digit on the test subset
+    # Predict the value of the digit on the test input image
     predicted = clf.predict(image)
 
     return str(predicted)
@@ -45,10 +45,9 @@ print("Client side- Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-#  Do 10 requests, waiting each time for a response
+#  send a request and wait for a response to make sure connection is ok
 request = 1
 print("Client-message-Sending request %s …" % request)
-request +=1
 socket.send(b"Client Ready to start receiving images to process")
 
 index = 1
@@ -68,5 +67,5 @@ while True:
     strng = base64.b64encode(bytes)
     socket.send(strng)
     
-    print("Client-message Sent processed image reply %s " % index)
+    print("Client-message Sent processed image label %s " % index)
     index += 1
